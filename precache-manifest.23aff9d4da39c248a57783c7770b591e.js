c418e3d047bd4388eaffed7674cb3017399ac198
mrkhmath <kh_math@ymail.com>
2020-08-29 14:03:11 +0400

    Updates

---
+++ precache-manifest.23aff9d4da39c248a57783c7770b591e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5618088447fea2e43c82154958dcebc9",
+    "url": "/school/index.html"
+  },
+  {
+    "revision": "843c4a40021481c9ab9e",
+    "url": "/school/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "37f0af9fcce387cadc31",
+    "url": "/school/static/js/2.b304a7ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/school/static/js/2.b304a7ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843c4a40021481c9ab9e",
+    "url": "/school/static/js/main.74f03b72.chunk.js"
+  },
+  {
+    "revision": "850ad2a0f679bf0c6158",
+    "url": "/school/static/js/runtime-main.ea8434de.js"
+  }
+]);
