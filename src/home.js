11183da53afdd8079b1d4b6bc86b3c8eb6761f52
mrkhmath <kh_math@ymail.com>
2020-08-29 13:39:09 +0400

    final

---
+++ src/home.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+const Home = () => {
+  const [input, setinput] = useState("");
+  const getdata = async (num) => {
+    try {
+      const body = { esis: num };
+      const response = await fetch("http://mutanabi.herokuapp.com/st", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      const jsonData = await response.json();
+      if (jsonData.grade === "still") {
+        document.getElementById("ename").innerHTML = jsonData.ename;
+        document.getElementById("edutype").innerHTML = "الطالب دور ثان";
+        document.getElementById("home").innerHTML = "";
+      } else {
+        if (jsonData.edutype === "DL") {
+          document.getElementById("edutype").innerHTML =
+            "تعلم عن بعد من المنزل";
+          document.getElementById("ename").innerHTML = jsonData.ename;
+          document.getElementById("home").innerHTML = jsonData.home;
+        } else {
+          document.getElementById("edutype").innerHTML =
+            "تعلم واقعي - من المدرسة";
+          document.getElementById("ename").innerHTML = jsonData.ename;
+          document.getElementById("home").innerHTML = jsonData.home;
+        }
+      }
+    } catch (err) {
+      document.getElementById("ename").innerHTML = "يرجي التأكد من رقم الطالب";
+      document.getElementById("edutype").innerHTML = "";
+      document.getElementById("home").innerHTML = "";
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="text-center mt-5">مدرسة المتنبي </h1>
+      <h3 className="text-center mt-5">ادخل رقم الطالب لمعرفة نوع التعلم</h3>
+      <div
+        className="group-form d-inline-flex p-2"
+        style={{ marginLeft: "35%", marginTop: "10%" }}
+      >
+        <input
+          type="text"
+          className="form-control"
+          onChange={(e) => setinput(e.target.value)}
+        />
+        <button className="btn btn-primary" onClick={() => getdata(input)}>
+          ابحث
+        </button>
+      </div>
+      <blockquote className="blockquote text-center mt-5">
+        <h5 id="ename"></h5>
+        <h5 id="home"></h5>
+        <h5 id="edutype"></h5>
+      </blockquote>
+    </div>
+  );
+};
+
+export default Home;
